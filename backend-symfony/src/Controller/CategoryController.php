232742ac3d8261aa416d84b072180bd3a84312b9
Sergei Johansen <sergei.johansen@protonmail.com>
2025-10-13 10:43:20 +0100

    add getbyid

--- backend-symfony/src/Controller/CategoryController.php
+++ backend-symfony/src/Controller/CategoryController.php
@@ -1,16 +1,56 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/categories', format: "json")]
 final class CategoryController extends AbstractController
 {
-    #[Route('/category')]
-    public function index(): JsonResponse
+    public function __construct(
+        private CategoryRepository $categoryRepository,
+    ) {
+    }
+
+    #[Route('/', methods: ['GET'])]
+    public function getAll(): JsonResponse
     {
-        return $this->json("", 201);
+        $categories = $this->categoryRepository->findAll();
+
+        return $this->json(compact('categories'), 201);
+    }
+
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function getById(string $id): JsonResponse
+    {
+        $category = $this->categoryRepository->findOneBy(["id", $id]);
+        if (!$category) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->json($id);
+    }
+
+    #[Route('/{id}', methods: ['PUT'])]
+    public function update(string $id): JsonResponse
+    {
+        return $this->json($id);
+    }
+
+    #[Route("/{id}", methods: ["DELETE"])]
+    public function delete(string $id): JsonResponse
+    {
+        $this->categoryRepository->createQueryBuilder('delete', 'd')
+            ->delete('categories', 'c')
+            ->where('id = :id')
+            ->setParameter('id', $id);
+
+        return $this->json('Deleted');
     }
 }
